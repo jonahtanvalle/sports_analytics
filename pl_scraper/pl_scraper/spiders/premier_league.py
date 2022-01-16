f55bbac20c19dcb07d918b32f9c7fead4de97fd0
jonahtanvalleqx <jonahtan.valle@quipux.com>
2022-01-16 12:21:00 -0500

    feat: modificación al xpath para la extracción de fechas.

--- pl_scraper/pl_scraper/spiders/premier_league.py
+++ pl_scraper/pl_scraper/spiders/premier_league.py
@@ -13,13 +13,13 @@ class PremierLeague(scrapy.Spider):
         score_xpath = "//*/section/div[3]/div/div/div[1]/div[2]/div/div/text()"
         home_team_xpath = "//*/section/div[3]/div/div/div[1]/div[1]/a[2]/span[1]/text()"
         visitor_team_xpath = "//*/section/div[3]/div/div/div[1]/div[3]/a[2]/span[1]/text()"
-        date_xpath = "//*/section[2]/div[2]/section/div[1]/div/div[1]/div[1]"
+        date_xpath = "/html/body/main/div/section[2]/div[2]/section/div[1]/div/div[1]/div/text()"
 
         # Getting response from web
         score = response.xpath(score_xpath).getall()
         home_team = response.xpath(home_team_xpath).get()
         visitor_team = response.xpath(visitor_team_xpath).get()
-        date = response.css("div.matchDate renderMatchDateContainer").get()
+        date = response.xpath(date_xpath).getall()
 
         yield {
             'home': home_team,
